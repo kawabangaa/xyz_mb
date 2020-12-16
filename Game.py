bdbc6694cb320d526a55f5f4481671cda31a032d
Yaron Scherf <yaronscherf@gmail.com>
2020-12-16 23:32:24 +0200

    end of day

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 import numpy as np
 from random import randint
-from constants import b_value, r_value, verbose
+from constants import b_value, r_value,d_value, verbose
 from rounds import invoke_animal, validate_board
 
 
@@ -22,6 +22,46 @@ class Game():
         ret, self.winner = validate_board(self.board)
         return ret
 
+    def play_turn(self, color, tactic = "random"):
+        if tactic == "random":
+            selected = self.moves_list[0]
+            self.board[selected[0]][selected[1]] = color
+            if len(self.moves_list) > 0:
+                self.moves_list = self.moves_list[1:]
+            else:
+                self.moves_list = []
+        elif tactic == "draw":
+            opposite_color = color * -1
+            #checking if in one of the columns there're 2 of the opposite color and a 0
+            cols_sum = self.board.sum(axis=0)
+            col_indices = self.board[:,np.where(cols_sum == opposite_color * 2)]
+            if col_indices.size > 0:
+                col_indices = np.where(col_indices.reshape((1,3))==d_value)
+                self.board[col_indices] = color
+                self.moves_list.remove([col_indices[0][0],col_indices[1][0]])
+                return
+            # checking if in one of the rows there're 2 of the opposite color and a 0
+            rows_sum = self.board.sum(axis=1)
+            row_indices = self.board[np.where(rows_sum == opposite_color * 2), :]
+            if row_indices.size > 0:
+                row_indices = np.where(row_indices.reshape((1, 3)) == d_value)
+                self.board[row_indices] = color
+                self.moves_list.remove([row_indices[0][0], row_indices[1][0]])
+                return
+            # checking if in one of the diagonals there're 2 of the opposite color and a 0
+            both_diag_indices = [[[0,0],[1,1],[2,2]],[[0,2],[1,1],[2,0]]]
+            for diag_indices in both_diag_indices:
+                diag_sum = 0
+                for indices in diag_indices:
+                    diag_sum += self.board[indices[0],indices[1]]
+                if diag_sum == opposite_color * 2:
+                    for indices in diag_indices:
+                        if self.board[indices[0],indices[1]] == d_value:
+                            self.board[indices[0], indices[1]] = color
+                            self.moves_list.remove([indices[0], indices[1]])
+                            return
+
+
     def play_round(self):
         available_board_values = [b_value, r_value]
         if len(self.moves_list) == 1:
@@ -32,8 +72,7 @@ class Game():
         elif len(self.moves_list) > 1:
             value_idx = randint(0, 1)
             for i in range(2):
-                selected = self.moves_list[0]
-                self.board[selected[0]][selected[1]] = available_board_values[value_idx]
+
                 value_idx += 1
                 value_idx %= 2
                 self.moves_list = self.moves_list[1:]
--- game_runner.py
+++ game_runner.py
@@ -1,43 +1,49 @@
 import csv
+import pandas as pd
 from Game import Game
 from Probability import Probability
-from constants import animal_cnt, num_of_animal, verbose
-def run_games():
+from constants import animal_cnt, num_of_animal, verbose, b_value, r_value, d_value
 
-    special_prob = Probability(2,[2,1])
-    loop_range = 8
-    step_size = 1
-    plays_per_comb = 5000
-    with open('result.csv', mode='w') as res_file:
-        res_writer = csv.writer(res_file)
-        for a in range(0,loop_range,step_size):
-            for b in range(0,loop_range,step_size):
-                for c in range(0,loop_range,step_size):
-                    for d in range(0,loop_range,step_size):
-                        res = {-1: 0, 0: 0, 1: 0}
-                        if verbose:
-                            print("starting with new params: "+ str(a) + str(b) + str(c) + str(d))
-                        csv_row = []
-                        csv_row.append(str(a))
-                        csv_row.append(str(b))
-                        csv_row.append(str(c))
-                        csv_row.append(str(d))
-                        animal_prob = Probability(num_of_animal,[a,b,c,d])
-                        game = Game(special_prob, animal_prob)
-                        for i in range(plays_per_comb):
-                            if verbose:
-                                print("starting game " + str(i))
-                            res[game.play_game()] += 1
-                            if verbose:
-                                print('result stake: ' + str(res))
-                            game.reset()
-                        factor = 100.0 / sum(res.values())
+def run_games(special_prob_dist, max_weight, weight_resulotion, plays_per_comb):
 
-                        for k in res:
-                            csv_row.append(str(res[k] * factor))
+    special_prob = Probability(2,special_prob_dist)
+
+    idx = 0
+    data = []
+    for wasp_weight in range(0, max_weight, weight_resulotion):
+        for chameleon_weight in range(0, max_weight, weight_resulotion):
+            for snake_weight in range(0, max_weight, weight_resulotion):
+                for cheetah_weight in range(0, max_weight, weight_resulotion):
+                    # Generating the games
+                    res = {b_value: 0, d_value: 0, r_value: 0}
+                    if verbose:
+                        print("starting with new params: "+ str(wasp_weight) + str(chameleon_weight) + str(snake_weight) + str(cheetah_weight))
+                    animal_prob = Probability(num_of_animal,[wasp_weight,chameleon_weight,snake_weight,cheetah_weight])
+                    game = Game(special_prob, animal_prob)
+                    for i in range(plays_per_comb):
                         if verbose:
-                            print("finished params, result: "+ str(csv_row))
-                        res_writer.writerow(csv_row)
+                            print("starting game " + str(i))
+                        res[game.play_game()] += 1
+                        if verbose:
+                            print('result stake: ' + str(res))
+                        game.reset()
+                    # Converting results into percentages
+                    factor = 100.0 / sum(res.values())
+                    for k in res:
+                        res[k] = res[k] * factor
+                    if verbose:
+                        print("finished params, result: "+ str(res))
+                    # Inserting the data into the data list
+                    data.append([wasp_weight, chameleon_weight, snake_weight, cheetah_weight, res[b_value], res[r_value], res[d_value]])
+                    idx += 1
+    df = pd.DataFrame(data, columns=['wasp', 'chameleon', 'snake', 'cheetah', 'black_per', 'red_per', 'draw_per'])
+    return df
+
+def plot_games():
+    df = run_games([2,1], 4, 1, 100)
+    df.to_csv('result.csv')
+
+
 def run_one_game():
     res = {-1: 0, 0: 0, 1: 0}
     plays_per_comb = 1000
@@ -63,4 +69,5 @@ def test_probability():
 if __name__ == '__main__':
     #test_probability()
     #run_one_game()
-    run_games()
+    #run_games([2,1], 8, 1, 5000)
+    plot_games()
