a4635609f067fd9599a01d5d8c54605f6ac44d73
Yaron Scherf <yaronscherf@gmail.com>
2020-12-20 03:22:33 +0200

    set the correct constants for play_xyz to match v03

--- play_xyz.py
+++ play_xyz.py
@@ -9,5 +9,5 @@ def play_xyz(s, d, b):
 if __name__ == '__main__':
     s = Probability(2,[2, 1])
     d = Probability(2,[59, 41])
-    b = Probability(4,[0.273, 0.364, 0.273, 0.09])
+    b = Probability(4,[4,3,3,3])
     play_xyz(s, d, b)
